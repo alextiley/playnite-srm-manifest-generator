3eea3411bc178cfcde97da727d61a59404fdb8b8
Alex Tiley <1131174+alextiley@users.noreply.github.com>
2023-11-12 09:09:25 +0000

    chore: improve logging and rename extension

--- PowerShellScript.psm1
+++ PowerShellScript.psm1
@@ -22,14 +22,26 @@ function Write-Manifest {
 
 function OnLibraryUpdated()
 {
-  # figure out which process is running
-  $PlayniteExe = Get-Process -Name "Playnite*" | Select-Object -ExpandProperty Path
-  $PlayniteDir = Split-Path -Path $PlayniteExe -Parent
+  $__logger.Info("library updated, fetching games from playnite api")
 
   # get all applicable games
   $AllGames = $PlayniteApi.Database.Games
   $VisibleGames = $AllGames | Where-Object {$_.Hidden -eq 0}
   $GamesWithLibraries = $VisibleGames | Select-Object Id, Name, PluginId, @{Name='Library'; Expr={ $PluginId = $_.PluginId; $PlayniteApi.Addons.Plugins | where { $_.Id -eq $PluginId } }}
+
+  $__logger.Info("${GamesWithLibraries.Count} games found")
+
+  # todo get the underlying play action so we can invoke that from steam directly
+  #
+
+  # figure out which process is running
+  $PlayniteExe = Get-Process -Name "Playnite*" | Select-Object -ExpandProperty Path
+  $PlayniteDir = Split-Path -Path $PlayniteExe -Parent
+  $DataDir = $CurrentExtensionDataPath
+
+  $__logger.Info("process path is $PlayniteExe")
+  $__logger.Info("install path is $PlayniteExe")
+  $__logger.Info("extension data path is $CurrentExtensionDataPath")
 
   # convert to SRM manifest format, but also `store` property for grouping later
   $ManifestsWithStores = $GamesWithLibraries | ForEach-Object {
@@ -42,8 +54,7 @@ function OnLibraryUpdated()
     }
   }
 
-  $__logger.Info("Writing manifests to $CurrentExtensionDataPath")
-
   # write manifest files for each store front
-  ,$ManifestsWithStores | Write-Manifest -OutputDir $CurrentExtensionDataPath
+  $__logger.Info("writing manifests to $DataDir")
+  ,$ManifestsWithStores | Write-Manifest -OutputDir $DataDir
 }
